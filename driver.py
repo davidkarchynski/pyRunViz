8cbe44b2a1f0a3bb774d20d73503fd4e2e7b7f97
David Karchynski <david.ckor@gmail.com>
2018-11-23 23:48:53 -0800

    Writing json output to a file.

--- driver.py
+++ driver.py
@@ -18,7 +18,7 @@ class Node:
         self.timespent = timespent
 
     def __repr__(self):
-        return " Children:" + str(len(self.children)) + " total_time: " + repr(self.total_time) + "average: " + repr(self.average_time) + "\n"
+        return " Children:" + str(len(self.children)) + " total_time: " + repr(self.total_time) + " average: " + repr(self.average_time) + "\n"
 
 def main():
     if len(sys.argv) != 2:
@@ -78,13 +78,12 @@ def parse_result(result):
     lines.reverse()
     lines.pop(0)
 
-    for line in lines:
-        print(line)
-    parse_input(lines)
-    # return create_json()
+    json_output = parse_input(lines)
+    write_json(json_output)
 
 def parse_input(lines):
     nodes = {}
+
     for line in lines:
         node_name, timespent = line.split('~')
         call_stack = node_name.split('|')
@@ -111,56 +110,61 @@ def parse_input(lines):
         times = list(map((lambda x: float(x)), node.timespent))
         node.total_time = reduce((lambda a, b: a + b), times)
         node.average_time = node.total_time / len(times)
-        node.times = []
+        del node.timespent
 
-    print(nodes)
+    return json.dumps(nodes["main"], default=lambda x: x.__dict__)
 
-# Helper methods for JSON
-def log_link(from_node, to_node):
-    links_list.append((from_node, to_node))
-
-def log_node(node, t):
-    nodes_list.append((node, t))
-
-def create_nodes_dictionary():
-    results = []
-
-    all_nodes = list(map((lambda x: x[0]), nodes_list))
-    all_nodes = list(set(all_nodes))
-
-    for n in all_nodes:
-        all_element_n = list(filter((lambda x: x[0] == n), nodes_list))
-        all_element_sizes = list(map((lambda x: float(x[1])), all_element_n))
-        sum_element_n = reduce((lambda a, b: a + b), all_element_sizes)
-        avg_element_n = sum_element_n / len(all_element_n)
-        results.append({"id": n, "average_time": avg_element_n, "total_time": sum_element_n})
-    return results
-
-def create_links_dictionary():
-    results = []
-    all_links = set(links_list)
-
-    for l in all_links:
-        count_l = links_list.count(l)
-        results.append({"source": l[0], "target": l[1], "value": count_l})
-    return results
-
-def create_json():
-    # s = inspect.stack()
-    # stack_str = ""
-
-    # for entry in s:
-    #     stack_str = entry.function + "->" + stack_str
-
-    # print(stack_str)
-
-    nodes_dictionary = create_nodes_dictionary()
-    links_dictionary = create_links_dictionary()
-    results_object = {
-        "nodes": nodes_dictionary,
-        "links": links_dictionary
-    }
-    results_json = json.dumps(results_object)
-    return results_json
+def write_json(data):
+    output_file = open("output.json", "w")
+    output_file.write(data)
+    output_file.close
 
 main()
+
+# # Helper methods for JSON
+# def log_link(from_node, to_node):
+#     links_list.append((from_node, to_node))
+
+# def log_node(node, t):
+#     nodes_list.append((node, t))
+
+# def create_nodes_dictionary():
+#     results = []
+
+#     all_nodes = list(map((lambda x: x[0]), nodes_list))
+#     all_nodes = list(set(all_nodes))
+
+#     for n in all_nodes:
+#         all_element_n = list(filter((lambda x: x[0] == n), nodes_list))
+#         all_element_sizes = list(map((lambda x: float(x[1])), all_element_n))
+#         sum_element_n = reduce((lambda a, b: a + b), all_element_sizes)
+#         avg_element_n = sum_element_n / len(all_element_n)
+#         results.append({"id": n, "average_time": avg_element_n, "total_time": sum_element_n})
+#     return results
+
+# def create_links_dictionary():
+#     results = []
+#     all_links = set(links_list)
+
+#     for l in all_links:
+#         count_l = links_list.count(l)
+#         results.append({"source": l[0], "target": l[1], "value": count_l})
+#     return results
+
+# def create_json():
+#     # s = inspect.stack()
+#     # stack_str = ""
+
+#     # for entry in s:
+#     #     stack_str = entry.function + "->" + stack_str
+
+#     # print(stack_str)
+
+#     nodes_dictionary = create_nodes_dictionary()
+#     links_dictionary = create_links_dictionary()
+#     results_object = {
+#         "nodes": nodes_dictionary,
+#         "links": links_dictionary
+#     }
+#     results_json = json.dumps(results_object)
+#     return results_json
